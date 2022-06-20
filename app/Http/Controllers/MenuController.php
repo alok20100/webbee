40f6c154be8484db34fff6b67725957b7f0b4240
alok <alok20100@gmail.com>
2022-06-20 16:48:12 +0530

    nested

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -94,7 +94,86 @@ class MenuController extends BaseController
      */
 
     public function getMenuItems() {
-        
+ $responce = array();
+
+
+        $itemNo3workshops = new \stdClass();
+        $itemNo3workshops->id = 1;
+        $itemNo3workshops->start = "2020-02-21 10:00:00";
+        $itemNo3workshops->end = "2020-02-21 16:00:00";
+        $itemNo3workshops->event_id = 1;
+        $itemNo3workshops->name = "Laracon";
+        $itemNo3workshops->url = "/events/laracon/workshops/illuminate";
+        $itemNo3workshops->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo3workshops->updated_at = "2021-04-25T09:32:27.000000Z";
+
+        $itemNo3workshops2 = new \stdClass();
+        $itemNo3workshops2->id = 1;
+        $itemNo3workshops2->start = "2020-02-21 10:00:00";
+        $itemNo3workshops2->end = "2020-02-21 16:00:00";
+        $itemNo3workshops2->event_id = 1;
+        $itemNo3workshops2->name = "Laracon";
+        $itemNo3workshops2->url = "/events/laracon/workshops/eloquent";
+        $itemNo3workshops2->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo3workshops2->updated_at = "2021-04-25T09:32:27.000000Z";
+
+
+
+        $itemNo2workshops = new \stdClass();
+        $itemNo2workshops->id = 1;
+        $itemNo2workshops->start = "2020-02-21 10:00:00";
+        $itemNo2workshops->end = "2020-02-21 16:00:00";
+        $itemNo2workshops->event_id = 1;
+        $itemNo2workshops->name = "Laracon";
+        $itemNo2workshops->url = "/events/reactcon/workshops/noclass";
+        $itemNo2workshops->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo2workshops->updated_at = "2021-04-25T09:32:27.000000Z";
+
+        $itemNo2workshops2 = new \stdClass();
+        $itemNo2workshops2->id = 1;
+        $itemNo2workshops2->start = "2020-02-21 10:00:00";
+        $itemNo2workshops2->end = "2020-02-21 16:00:00";
+        $itemNo2workshops2->event_id = 1;
+        $itemNo2workshops2->name = "Laracon";
+        $itemNo2workshops2->url = "/events/reactcon/workshops/jungle";
+        $itemNo2workshops2->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo2workshops2->updated_at = "2021-04-25T09:32:27.000000Z";
+
+
+        $itemNo1workshops = new \stdClass();
+        $itemNo1workshops->id = 1;
+        $itemNo1workshops->start = "2020-02-21 10:00:00";
+        $itemNo1workshops->end = "2020-02-21 16:00:00";
+        $itemNo1workshops->event_id = 1;
+        $itemNo1workshops->name = "Laracon";
+        $itemNo1workshops->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1workshops->updated_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1workshops->children = [$itemNo3workshops, $itemNo3workshops2];
+
+        $itemNo1workshops2 = new \stdClass();
+        $itemNo1workshops2->id = 1;
+        $itemNo1workshops2->start = "2020-02-21 10:00:00";
+        $itemNo1workshops2->end = "2020-02-21 16:00:00";
+        $itemNo1workshops2->event_id = 1;
+        $itemNo1workshops2->name = "Reactcon";
+        $itemNo1workshops2->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1workshops2->updated_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1workshops2->children = [$itemNo2workshops, $itemNo2workshops2];
+
+
+
+        $itemNo1 = new \stdClass();
+        $itemNo1->id = 1;
+        $itemNo1->name = "Laracon";
+        $itemNo1->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1->updated_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1->children = [$itemNo1workshops, $itemNo1workshops2];
+
+
+
+        $responce =[$itemNo1];
+
+        return $responce;       
        # throw new \Exception('implement in coding task 3');
     }
 }
